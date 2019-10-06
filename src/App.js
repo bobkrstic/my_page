b80392763d044b5646f6bea86fa981dbf19f5614
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-06 15:35:50 -0700

    initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container center">
+        <nav className="menu">
+          <h1 className="menu__logo">WoWeWeb</h1>
+
+          <div className="menu__right">
+            <ul className="menu__list">
+              <li className="menu__list-item">
+                <a className="menu__link menu__link--active" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__link" href="#">
+                  Portfolio
+                </a>
+              </li>
+              <li className="menu__list-item">
+                <a className="menu__link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+
+            <button className="menu__search-button"></button>
+
+            <form className="menu__search-form hide" method="POST">
+              <input
+                className="menu__search-input"
+                placeholder="Type and hit enter"
+              />
+            </form>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default App;
