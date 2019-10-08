2e3a40fcdda54dd7422f2590d268c6ba6e524a4a
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-07 17:45:11 -0700

    Setting up files, adding Router and MDL

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Body from "./components/Body";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Body />
-        <Footer />
+      <div style={{ height: "300px", position: "relative" }}>
+        <Layout fixedHeader>
+          <Header
+            title={
+              <span>
+                <span style={{ color: "#ddd" }}>Area / </span>
+                <strong>The Title</strong>
+              </span>
+            }
+          >
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content />
+        </Layout>
       </div>
     );
   }
