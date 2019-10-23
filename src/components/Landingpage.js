a39634057ad6f41e03ee7dabbff189975bf5b2ad
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-23 00:07:18 -0700

    Adding new backgrounds

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
+import background from "./background4.jpg";
 
 class Landingpage extends Component {
   render() {
     return (
       <div style={{ width: "100%", margin: "auto" }}>
-        <Grid className="landing-grid">
+        <Grid
+          className="landing-grid"
+          style={{ backgroundImage: `url(${background})` }}
+        >
           <Cell col={12}>
             <img
               src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/malecostume-512.png"
